4a950388063969ec83611a322209b2e0033da11e
tnghgks <tnghgks@naver.com>
2022-12-09 16:18:24 +0900

    feat: NPM 패키지 설치 및 에셋 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyled = createGlobalStyle`
+${reset} // 초기화css
+
+@font-face {
+  font-family: 'Godo';
+  font-style: normal;
+  font-weight: 400;
+  src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoM.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoM.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Godo';
+  font-style: normal;
+  font-weight: 700;
+  src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff2') format('woff2'), url('//cdn.jsdelivr.net/korean-webfonts/1/corps/godo/Godo/GodoB.woff') format('woff');
+}
+
+//css변수를 만들어서 쓸려고 합니다.
+:root{
+  --mainColor:#1A374D;
+  --subColor:#406882;
+  --accentColor:#6998AB;
+  --lightColor:#B1D0E0;
+
+  --fontColor:#ffffff;
+}
+html{
+  font-size:10px;
+}
+
+body{
+  font-family: 'Godo', sans-serif;
+}
+`;
+
 function App() {
-  return <div>초기화면</div>;
+  return (
+    <>
+      <GlobalStyled />
+      <div>홈</div>
+    </>
+  );
 }
 
 export default App;
