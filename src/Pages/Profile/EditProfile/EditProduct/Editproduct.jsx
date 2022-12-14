be809bf0616d9397b42492cd74ec0e1c156953ac
JongSeoIM <k10seo@likelion.org>
2022-12-14 17:19:43 +0900

    feat: nav바 적용 EditUserProfile, EditProduct nav바 적용 design: padding 적용

--- src/Pages/Profile/EditProfile/EditProduct/Editproduct.jsx
+++ src/Pages/Profile/EditProfile/EditProduct/Editproduct.jsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
-import ImgButton from "../../../Components/ImgButton";
-import CommonInput from "../../../Components/CommonInput";
+import ImgButton from "../../../../Components/ImgButton";
+import CommonInput from "../../../../Components/CommonInput";
+import BasicNav from "../../../../Components/Navbar/UploadNav";
 
 // 페이지 전체 컨테이너 컴퍼넌트
 const EditProfileContainer = styled.main`
   width: 390px;
   height: 820px;
+  padding: 78px 34px 0 34px;
   margin: 0 auto;
 `;
 
 // 상품 사진 변경 컴퍼넌트
 
 const ProductContainer = styled.section`
-  width: 332px;
+  width: 322px;
   margin-top: 30px;
   color: #767676;
   font-weight: 400;
@@ -43,32 +45,34 @@ const InputContainer = styled.section`
 
 function EditProduct() {
   return (
-    <EditProfileContainer>
-      <headr>navbar</headr>
-      <ProductContainer>
-        <p>이미지 등록</p>
-        <EditProductImgContainer>
-          <ImgBtn />
-        </EditProductImgContainer>
-      </ProductContainer>
-      <InputContainer>
-        <CommonInput
-          name="상품명"
-          type="text"
-          placeholder={"2~15자 이내여야 합니다."}
-        />
-        <CommonInput
-          name="가격"
-          type="text"
-          placeholder={"숫자만 입력 가능 합니다."}
-        />
-        <CommonInput
-          name="판매링크"
-          type="text"
-          placeholder={"URl을 입력해 주세요."}
-        />
-      </InputContainer>
-    </EditProfileContainer>
+    <>
+      <BasicNav />
+      <EditProfileContainer>
+        <ProductContainer>
+          <p>이미지 등록</p>
+          <EditProductImgContainer>
+            <ImgBtn />
+          </EditProductImgContainer>
+        </ProductContainer>
+        <InputContainer>
+          <CommonInput
+            name="상품명"
+            type="text"
+            placeholder={"2~15자 이내여야 합니다."}
+          />
+          <CommonInput
+            name="가격"
+            type="text"
+            placeholder={"숫자만 입력 가능 합니다."}
+          />
+          <CommonInput
+            name="판매링크"
+            type="text"
+            placeholder={"URl을 입력해 주세요."}
+          />
+        </InputContainer>
+      </EditProfileContainer>
+    </>
   );
 }
 
--- src/Pages/Profile/EditProfile/EditUserProfile/EditUserProfile.jsx
+++ src/Pages/Profile/EditProfile/EditUserProfile/EditUserProfile.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
-import BasicProfileImg from "../../../Components/BasicProfileImg";
-import ImgButton from "../../../Components/ImgButton";
-import CommonInput from "../../../Components/CommonInput";
+import BasicProfileImg from "../../../../Components/BasicProfileImg";
+import ImgButton from "../../../../Components/ImgButton";
+import CommonInput from "../../../../Components/CommonInput";
+import BasicNav from "../../../../Components/Navbar/UploadNav";
 
 // 페이지 전체 컨테이너 컴퍼넌트
 const EditProfileContainer = styled.main`
   width: 390px;
   height: 820px;
   margin: 0 auto;
+  padding: 78px 34px 0 34px;
 `;
 
 // 프로필 사진 변경 컴퍼넌트
@@ -42,32 +44,34 @@ const InputContainer = styled.section`
 
 function EditUserProfile() {
   return (
-    <EditProfileContainer>
-      <headr>navbar</headr>
-      <ProfileContainer>
-        <EditImgContainer>
-          <BasicProfileImg src="" alt="" />
-          <ImgBtn src="" alt="" />
-        </EditImgContainer>
-      </ProfileContainer>
-      <InputContainer>
-        <CommonInput
-          name="사용자 이름"
-          type="text"
-          placeholder={"2~10자 이내여야 합니다."}
-        />
-        <CommonInput
-          name="계정 ID"
-          type="text"
-          placeholder={"영문,숫자,특수문자(.),(_))만 사용 가능합니다."}
-        />
-        <CommonInput
-          name="소개"
-          type="text"
-          placeholder={"자신과 판매할 상품에 대해 소개해 주세요!"}
-        />
-      </InputContainer>
-    </EditProfileContainer>
+    <>
+      <BasicNav />
+      <EditProfileContainer>
+        <ProfileContainer>
+          <EditImgContainer>
+            <BasicProfileImg src="" alt="" />
+            <ImgBtn src="" alt="" />
+          </EditImgContainer>
+        </ProfileContainer>
+        <InputContainer>
+          <CommonInput
+            name="사용자 이름"
+            type="text"
+            placeholder={"2~10자 이내여야 합니다."}
+          />
+          <CommonInput
+            name="계정 ID"
+            type="text"
+            placeholder={"영문,숫자,특수문자(.),(_))만 사용 가능합니다."}
+          />
+          <CommonInput
+            name="소개"
+            type="text"
+            placeholder={"자신과 판매할 상품에 대해 소개해 주세요!"}
+          />
+        </InputContainer>
+      </EditProfileContainer>
+    </>
   );
 }
 
