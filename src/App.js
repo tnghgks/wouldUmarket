352e145f2185120f0f56992b5d844dd95aa403bc
tnghgks <tnghgks@naver.com>
2022-12-14 17:07:42 +0900

    design: 링크 색상 수정과 방문 후 링크 색상 수정

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ button {
   margin:0;
   padding:0;
 }
+a{
+  color:inherit;
+  text-decoration: none;
+}
+a:visited{
+  color:inherit;
+}
+
 .ir-hidden {
    position: absolute;
    clip: rect(0 0 0 0);
