c15967ebcedb5c6586350220eaf4bdf4a4cffb34
tnghgks <tnghgks@naver.com>
2022-12-14 16:33:21 +0900

    design: UserProduects 컴포넌트 디자인 수정

--- src/Pages/Profile/UserProfile/UserProducts/UserProducts.jsx
+++ src/Pages/Profile/UserProfile/UserProducts/UserProducts.jsx
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 import Product from "../../../../Components/Product";
 
-const Container = styled.section`
+const Container = styled.article`
   width: 100%;
   padding: 20px 0px 20px 16px;
   background-color: #ffffff;
-  overflow: hidden;
   margin-bottom: 6px;
   border: 0.5px solid #dbdbdb;
+  display: flex;
+  justify-content: center;
+`;
+const ProductContainer = styled.div`
+  width: 390px;
+  overflow: scroll hidden;
 `;
 
 const Title = styled.h2`
@@ -26,12 +31,14 @@ const ProductsList = styled.ul`
 function UserProducts() {
   return (
     <Container>
-      <Title>판매 중인 상품</Title>
-      <ProductsList>
-        <Product />
-        <Product />
-        <Product />
-      </ProductsList>
+      <ProductContainer>
+        <Title>판매 중인 상품</Title>
+        <ProductsList>
+          <Product />
+          <Product />
+          <Product />
+        </ProductsList>
+      </ProductContainer>
     </Container>
   );
 }
