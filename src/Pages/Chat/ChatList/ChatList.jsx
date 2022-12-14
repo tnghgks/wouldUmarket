dd25715dbedd012a3f43cd46ea30f4093b4955b5
JongSeoIM <k10seo@likelion.org>
2022-12-14 23:44:49 +0900

    feat: ChatList 컴포넌트 생성
    ChatList 컴포넌트 생성

--- src/Pages/Chat/ChatList/ChatList.jsx
+++ src/Pages/Chat/ChatList/ChatList.jsx
@@ -1,56 +1,13 @@
 import styled from "styled-components";
-import profileImg from "../../../assets/Ellipse-1.png";
-import BasicNav from "../../../Components/BasicNav";
+import BasicNav from "../../../Components/Navbar/BasicNav";
+import TabMenu from "../../../Components/TabMenu";
+import ChatlistProfile from "../../../Components/ChatlistProfile";
 
 const ChatListContainer = styled.main`
   width: 390px;
   height: 712px;
+  padding: 72px 16px 0 16px;
   margin: 0 auto;
-  position: relative;
-`;
-
-const Img = styled.img`
-  width: 42px;
-  height: 42px;
-  position: relative;
-`;
-
-const CheckOnline = styled.section`
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  background-color: var(--subColor);
-  position: absolute;
-  top: 0;
-`;
-
-const UserSerchContainer = styled.li`
-  width: 100%;
-  height: 42px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 12px;
-  font-size: 1.4rem;
-  font-family: "godo", sans-serif;
-`;
-
-const UserFollowSmall = styled.p`
-  display: inline-block;
-  font-size: 1.2rem;
-  margin-top: 4px;
-  font-family: "LINESeedKR-Bd", sans-serif;
-  color: #767676;
-`;
-
-const ChatDateSpan = styled.span`
-  position: absolute;
-  right: 0;
-  color: #dbdbdb;
-  font-family: "LINESeedKR-Bd", sans-serif;
-  font-weight: 400;
-  font-size: 10px;
-  line-height: 13px;
 `;
 
 function ChatList() {
@@ -58,20 +15,26 @@ function ChatList() {
     <>
       <BasicNav />
       <ChatListContainer>
-        <ul>
-          <UserSerchContainer>
-            <Img src={profileImg} />
-            <CheckOnline />
-            <div>
-              <p>애월읍 위니브 감귤 농장</p>
-              <UserFollowSmall>
-                <small>이번에 정정 언제하맨 마씸?</small>
-                <ChatDateSpan>2022.12.14</ChatDateSpan>
-              </UserFollowSmall>
-            </div>
-          </UserSerchContainer>
-        </ul>
+        <ChatlistProfile
+          newchat="new"
+          children="멋쟁이사자처럼 likelion"
+          chat="리덕스가 뭐죠? "
+          listchatdate="2022.12.14"
+        />
+        <ChatlistProfile
+          newchat="new"
+          children="개발자가 되고싶은 사람들"
+          chat="뭐부터 공부하죠?"
+          listchatdate="2069.06.27"
+        />
+        <ChatlistProfile
+          newchat=""
+          children="칠드런 프젝7조"
+          chat="화이이이팅!"
+          listchatdate="2022.12.09"
+        />
       </ChatListContainer>
+      <TabMenu />
     </>
   );
 }
