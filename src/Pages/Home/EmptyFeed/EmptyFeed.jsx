096aa3221f7c3a985e5e14b4812686fe20771f0b
seoyeon <wjddowkd456@naver.com>
2022-12-14 13:49:34 +0900

    design: EmptyFeed 컴포넌트에 MainNav, TabMenu 추가

--- src/Pages/Home/EmptyFeed/EmptyFeed.jsx
+++ src/Pages/Home/EmptyFeed/EmptyFeed.jsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 import SymbolLogoGray from "../../../Components/SymbolLogoGray";
 import CommonButton from "../../../Components/button/CommonButton";
+import MainNav from "../../../Components/MainNav";
+import TabMenu from "../../../Components/TabMenu";
 
-const Container = styled.section`
+const FeedContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
   min-height: 100vh;
+  gap: 20px;
 `;
 
 const Desc = styled.p`
@@ -18,11 +20,15 @@ const Desc = styled.p`
 
 function EmptyFeed() {
   return (
-    <Container>
-      <SymbolLogoGray />
-      <Desc>유저를 검색해 팔로우 해보세요!</Desc>
-      <CommonButton size="md" bgColor="main" children="검색하기" />
-    </Container>
+    <>
+      <MainNav titleContent="우주쉐어 피드" />
+      <FeedContainer>
+        <SymbolLogoGray />
+        <Desc>유저를 검색해 팔로우 해보세요!</Desc>
+        <CommonButton size="md" bgColor="main" children="검색하기" />
+      </FeedContainer>
+      <TabMenu />
+    </>
   );
 }
 
