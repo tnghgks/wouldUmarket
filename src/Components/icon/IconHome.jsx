33f739e1df9ed7be20d64299e1e89e3aa07fdadd
tnghgks <tnghgks@naver.com>
2022-12-15 12:01:34 +0900

    faet: Icon pathname에 따라 변경되도록 기능추가

--- src/Components/icon/IconHome.jsx
+++ src/Components/icon/IconHome.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import iconHome from "../../assets/icon/icon-home.svg";
+import iconHomeFill from "../../assets/icon/icon-home-fill.svg";
 
-const Img = styled.img`
+const Icon = styled.div`
   width: 24px;
   height: 24px;
+  background-image: url(${(props) => (props.isOn ? iconHomeFill : iconHome)});
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
-function IconHome({ className }) {
-  return <Img className={className} src={iconHome} alt="홈 아이콘" />;
+function IconHome({ className, pathname }) {
+  let isOn = false;
+  if (pathname === "/") {
+    isOn = true;
+  }
+  return <Icon className={className} isOn={isOn} />;
 }
 export default IconHome;
--- src/Components/icon/IconUser.jsx
+++ src/Components/icon/IconUser.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
-import iconUser from "../../assets/icon/icon-user.png";
+import iconUser from "../../assets/icon/icon-user.svg";
+import iconUserFill from "../../assets/icon/icon-user-fill.png";
 
-const Img = styled.img`
+const Icon = styled.div`
   width: 24px;
   height: 24px;
+  background-image: url(${(props) => (props.isOn ? iconUserFill : iconUser)});
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
-function IconUser({ className }) {
-  return <Img className={className} src={iconUser} alt="유저 아이콘" />;
+function IconUser({ className, pathname }) {
+  let isOn = false;
+  if (pathname === "/profile/myProfile") {
+    isOn = true;
+  }
+  return <Icon className={className} isOn={isOn} />;
 }
 export default IconUser;
