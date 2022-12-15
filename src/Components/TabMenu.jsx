b0c698afe2a13b954a4e0425ff3e128c119a0dd9
tnghgks <tnghgks@naver.com>
2022-12-15 12:03:30 +0900

    feat: Tabmenu pathname에 따른 프롭스 전달과 링크추가

--- src/Components/TabMenu.jsx
+++ src/Components/TabMenu.jsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 import IconHome from "./icon/IconHome";
 import IconMessageCircle from "./icon/IconMessageCircle";
 import IconEdit from "./icon/IconEdit";
 import IconUser from "./icon/IconUser";
+import Home from "../Pages/Home/Home";
+import ChatList from "../Pages/Chat/ChatList/ChatList";
+import PostUpload from "../Pages/Post/PostUpload/PostUpload";
+import Profile from "../Pages/Profile/UserProfile/Profile";
 
 const MenuContainer = styled.footer`
   width: 100%;
@@ -10,6 +15,7 @@ const MenuContainer = styled.footer`
   border-top: 0.5px solid #dbdbdb;
   bottom: 0%;
   padding: 12px 0 10px;
+  background-color: #ffffff;
 `;
 
 const ListContainer = styled.ol`
@@ -29,26 +35,36 @@ const ItemContainer = styled.li`
 `;
 
 function TabMenu() {
+  const { pathname } = useLocation();
+
   return (
     <MenuContainer>
       <nav>
         <ListContainer>
-          <ItemContainer>
-            <IconHome />
-            <p>홈</p>
-          </ItemContainer>
-          <ItemContainer>
-            <IconMessageCircle />
-            <p>채팅</p>
-          </ItemContainer>
-          <ItemContainer>
-            <IconEdit />
-            <p>게시물 작성</p>
-          </ItemContainer>
-          <ItemContainer>
-            <IconUser />
-            <p>프로필</p>
-          </ItemContainer>
+          <Link to={"/"} element={<Home />}>
+            <ItemContainer>
+              <IconHome pathname={pathname} />
+              <p>홈</p>
+            </ItemContainer>
+          </Link>
+          <Link to={"/chat/chatList"} element={<ChatList />}>
+            <ItemContainer>
+              <IconMessageCircle pathname={pathname} />
+              <p>채팅</p>
+            </ItemContainer>
+          </Link>
+          <Link to={"/post/postUpload"} element={<PostUpload />}>
+            <ItemContainer>
+              <IconEdit />
+              <p>게시물 작성</p>
+            </ItemContainer>
+          </Link>
+          <Link to={"/profile/myProfile"} element={<Profile />}>
+            <ItemContainer>
+              <IconUser pathname={pathname} />
+              <p>프로필</p>
+            </ItemContainer>
+          </Link>
         </ListContainer>
       </nav>
     </MenuContainer>
