1765fd612102f4b2786e350a2359e0a524ca196d
seoyeon <wjddowkd456@naver.com>
2022-12-16 18:51:54 +0900

    feat: 로그인Input 데이터 저장 구현

--- src/Pages/Login/LoginEmail.jsx
+++ src/Pages/Login/LoginEmail.jsx
@@ -6,7 +6,7 @@ import CommonInput from "../../Components/CommonInput";
 import { useDispatch, useSelector } from "react-redux";
 import { SET_TOKEN } from "../../store/Auth";
 
-const Container = styled.div`
+const Container = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -21,7 +21,7 @@ const Container = styled.div`
     font-weight: 500;
   }
 
-  & > div.inputContainer {
+  & > .inputContainer {
     position: relative;
     width: 100%;
     margin-bottom: 30px;
@@ -59,39 +59,67 @@ const Container = styled.div`
   }
 `;
 
+const Fieldset = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > button {
+    margin-bottom: 20px;
+    cursor: pointer;
+    &:hover {
+      background-color: var(--mainColor);
+      transition: background-color 200ms;
+    }
+  }
+`;
+
 function LoginEmail() {
   const dispatch = useDispatch();
   const { accessToken } = useSelector((state) => state.auth);
   console.log(accessToken);
 
-  const test = {
-    user: {
-      email: "tnghgks@naver.com",
-      password: "tnghgks123",
-    },
-  };
-
-  useEffect(() => {
-    (async function () {
-      const response = await fetch(`https://mandarin.api.weniv.co.kr/user/login`, {
+  async function getLoginData(inputData) {
+    const response = await fetch(
+      `https://mandarin.api.weniv.co.kr/user/login`,
+      {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(test),
-      });
-      const { user } = await response.json();
-      dispatch(SET_TOKEN(user.token));
-    })();
-  }, []);
+        body: JSON.stringify(inputData),
+      }
+    );
+    const { user } = await response.json();
+    dispatch(SET_TOKEN(user.token));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = Array.from(e.target.querySelectorAll("input")).map(
+      (item) => item.value
+    );
+    console.log(data);
+
+    const inputData = {
+      user: {
+        email: data[0],
+        password: data[1],
+      },
+    };
+    getLoginData(inputData);
+  }
 
   return (
     <Container>
       <h1 className="title">로그인</h1>
-      <div className="inputContainer">
-        <CommonInput name="이메일" type="email" />
-        <CommonInput name="비밀번호" type="password"></CommonInput>
-        <p className="warning ">*이메일 또는 비밀번호가 일치하지 않습니다.</p>
-      </div>
-      <CommonButton size="lg" bgColor="light" children="다음" />
+      <form className="inputContainer" onSubmit={handleSubmit}>
+        <Fieldset>
+          <legend className="ir-hidden">로그인</legend>
+          <CommonInput name="이메일" type="email" />
+          <CommonInput name="비밀번호" type="password" />
+          <p className="warning ">*이메일 또는 비밀번호가 일치하지 않습니다.</p>
+          <CommonButton size="lg" bgColor="light" children="다음" />
+        </Fieldset>
+      </form>
       <Link>이메일로 회원가입</Link>
     </Container>
   );
