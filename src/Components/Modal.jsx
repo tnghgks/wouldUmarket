8603cd80ecc7f947225f5a399547c8c4c9062ce8
tnghgks <tnghgks@naver.com>
2022-12-18 22:13:20 +0900

    feat: Modal 컴포넌트 영역 밖 클릭 시 닫힘기능 추가

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,14 +1,17 @@
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Container = styled.section`
   width: 100%;
-  height: 300px;
   background: #ffffff;
   border-radius: 10px 10px 0 0;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-bottom: 10px;
+  position: fixed;
+  bottom: 0px;
+  z-index: 10;
 `;
 const Bar = styled.div`
   width: 50px;
@@ -27,25 +30,50 @@ const ItemList = styled.ul`
     line-height: 1.8rem;
   }
 `;
+const BackBlur = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 /**
  *
- * @param {{modalInfo: [ {text: string ; handleFunc: function ;} ] }} param0
+ * @param {{modalInfo: [ {text: string ; handleFunc: function ;},isOpen:boolean;setIsOpen:function ] }} param0
  * @returns
  */
 
-function Modal({ modalInfo = [] }) {
+function Modal({ modalInfo = [], isOpen, setIsOpen }) {
+  const backBlur = useRef();
+
+  const modalCloseHandler = ({ target }) => {
+    if (isOpen && backBlur.current.contains(target)) setIsOpen(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", modalCloseHandler);
+    return () => {
+      window.removeEventListener("click", modalCloseHandler);
+    };
+  });
+
   return (
-    <Container>
-      <Bar />
-      <ItemList>
-        {modalInfo &&
-          modalInfo.map((modalItem, index) => (
-            <li key={index} onClick={modalItem.handleFunc}>
-              {modalItem.text}
-            </li>
-          ))}
-      </ItemList>
-    </Container>
+    <>
+      <Container>
+        <Bar />
+        <ItemList>
+          {modalInfo &&
+            modalInfo.map((modalItem, index) => (
+              <li key={index} onClick={modalItem.handleFunc}>
+                {modalItem.text}
+              </li>
+            ))}
+        </ItemList>
+      </Container>
+      <BackBlur ref={backBlur} />
+    </>
   );
 }
 export default Modal;
