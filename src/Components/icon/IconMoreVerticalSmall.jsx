fcf7d97eea7927f79d8ada600d9c50cbf05e964f
tnghgks <tnghgks@naver.com>
2022-12-18 22:14:19 +0900

    feat: Icon 클릭 시 onClick함수 실행 기능 추가

--- src/Components/icon/IconMoreVerticalSmall.jsx
+++ src/Components/icon/IconMoreVerticalSmall.jsx
@@ -4,9 +4,10 @@ import iconMore from "../../assets/icon/s-icon-more-vertical.png";
 const Img = styled.img`
   width: 18px;
   height: 18px;
+  cursor: pointer;
 `;
 
-function IconMoreVerticalSmall({ className }) {
-  return <Img className={className} src={iconMore} alt="작은 더보기 수직 아이콘" />;
+function IconMoreVerticalSmall({ className, onClick }) {
+  return <Img className={className} src={iconMore} alt="작은 더보기 수직 아이콘" onClick={onClick} />;
 }
 export default IconMoreVerticalSmall;
