08f7f712c3538f2605c58c07bfb3f3fc0d0dc886
tnghgks <tnghgks@naver.com>
2022-12-19 11:36:18 +0900

    feat: UserInfo follow,unfollow 기능 추가

--- src/Pages/Profile/UserProfile/UserInfo/UserInfo.jsx
+++ src/Pages/Profile/UserProfile/UserInfo/UserInfo.jsx
@@ -2,9 +2,115 @@ import styled from "styled-components";
 import BasicProfileImg from "../../../../Components/BasicProfileImg";
 import CommonButton from "../../../../Components/button/CommonButton";
 import IconMessageCircle from "../../../../Components/icon/IconMessageCircle";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import IconShare from "../../../../Components/icon/IconShare";
 import { useEffect, useState } from "react";
+
+function UserInfo({ user }) {
+  const navigate = useNavigate();
+  const { accountname } = useParams();
+  const [data, setData] = useState({});
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTZlODNmMTdhZTY2NjU4MWMzNTJkZCIsImV4cCI6MTY3NjU0NTI4NSwiaWF0IjoxNjcxMzYxMjg1fQ.SQif90hSbfq7Rvl6Ge5dXG6Y_h9CF7M1lTwda8V4aT8";
+
+  async function getUserData() {
+    try {
+      const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      });
+      const { profile } = await res.json();
+      if (!profile) return navigate("/feed");
+      setData(profile);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleFollowBtn() {
+    try {
+      const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}/follow`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      });
+      const { profile, message } = await res.json();
+      if (!profile) return alert(message);
+      getUserData();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function handleUnFollowBtn() {
+    try {
+      const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}/unfollow`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      });
+      const { profile, message } = await res.json();
+      if (!profile) return alert(message);
+      getUserData();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  return (
+    data && (
+      <Container>
+        <RowContainer>
+          <Link>
+            <FollowerInfo>
+              <span>{data.followerCount}</span>
+              <span>followers</span>
+            </FollowerInfo>
+          </Link>
+          <BasicProfileImg src={data.image} />
+          <Link>
+            <FollowingInfo>
+              <span>{data.followingCount}</span>
+              <span>followings</span>
+            </FollowingInfo>
+          </Link>
+        </RowContainer>
+        <UserNicname>{data.username}</UserNicname>
+        <UserId>@ {data.accountname}</UserId>
+        <UserDiscription>{data.intro}</UserDiscription>
+        <BtnContainer>
+          <LinkBtn to="/chat/id">
+            <IconMessageCircle />
+          </LinkBtn>
+          {data.isfollow ? (
+            <CommonButton size="md" fontColor="#767676" bgColor="white" event={handleUnFollowBtn}>
+              언팔로우
+            </CommonButton>
+          ) : (
+            <CommonButton size="md" event={handleFollowBtn}>
+              팔로우
+            </CommonButton>
+          )}
+
+          <LinkBtn>
+            <IconShare />
+          </LinkBtn>
+        </BtnContainer>
+      </Container>
+    )
+  );
+}
+export default UserInfo;
 
 const Container = styled.section`
   width: 100%;
@@ -89,75 +195,3 @@ const LinkBtn = styled(Link)`
     height: 20px;
   }
 `;
-function UserInfo({ user }) {
-  const [data, setData] = useState({
-    _id: "62e4333917ae66658100b69a",
-    username: "김마타",
-    accountname: "mataa",
-    intro: "하쿠나마타타",
-    image: "https://mandarin.api.weniv.co.kr/1659122486661.jpeg",
-    isfollow: false,
-    following: ["62d9f51017ae66658184fb47", "62e3407717ae666581dd9d78", "62e4ac1117ae6665810b6057"],
-    follower: ["62e435d317ae666581010a2f", "62b0237a5361aaea569aa879", "62e4ac1117ae6665810b6057", "63801e2617ae666581be782d"],
-    followerCount: 4,
-    followingCount: 3,
-  });
-
-  // useEffect(() => {
-  //   (async function () {
-  //     try {
-  //       const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/mataa`, {
-  //         method: "GET",
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //           "Content-type": "application/json",
-  //         },
-  //       });
-  //       const { profile } = await res.json();
-  //       setData(profile);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   })();
-  // }, []);
-
-  return (
-    <Container>
-      <RowContainer>
-        <Link>
-          <FollowerInfo>
-            <span>{data.followerCount}</span>
-            <span>followers</span>
-          </FollowerInfo>
-        </Link>
-        <BasicProfileImg src={data.image} />
-        <Link>
-          <FollowingInfo>
-            <span>{data.followingCount}</span>
-            <span>followings</span>
-          </FollowingInfo>
-        </Link>
-      </RowContainer>
-      <UserNicname>{data.username}</UserNicname>
-      <UserId>@ {data.accountname}</UserId>
-      <UserDiscription>{data.intro}</UserDiscription>
-      <BtnContainer>
-        <LinkBtn to="/chat/id">
-          <IconMessageCircle />
-        </LinkBtn>
-        {data.isfollow ? (
-          <CommonButton size="md" fontColor="#767676" bgColor="white">
-            언팔로우
-          </CommonButton>
-        ) : (
-          <CommonButton size="md">팔로우</CommonButton>
-        )}
-
-        <LinkBtn>
-          <IconShare />
-        </LinkBtn>
-      </BtnContainer>
-    </Container>
-  );
-}
-export default UserInfo;
