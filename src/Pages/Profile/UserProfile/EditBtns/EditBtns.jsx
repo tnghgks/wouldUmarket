8d63a0282b51c5fcfb484819b1a90dbf5a42d54d
tnghgks <tnghgks@naver.com>
2022-12-19 23:03:06 +0900

    feat: 수정버튼과 팔로우버튼 분리

---
+++ src/Pages/Profile/UserProfile/EditBtns/EditBtns.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import CommonButton from "../../../../Components/button/CommonButton";
+
+function EditBtns() {
+  return (
+    <BtnContainer>
+      <Link to="/profile/editProfile">
+        <CommonButton size="md" fontColor="#767676" bgColor="white">
+          프로필 수정
+        </CommonButton>
+      </Link>
+      <Link to="/profile/editProduct">
+        <CommonButton size="md" fontColor="#767676" bgColor="white">
+          상품등록
+        </CommonButton>
+      </Link>
+    </BtnContainer>
+  );
+}
+export default EditBtns;
+
+const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 24px;
+  margin-bottom: 26px;
+`;
