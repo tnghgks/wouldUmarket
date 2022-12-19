9f140b31948bfd0df350a3c1f58159df9620b564
tnghgks <tnghgks@naver.com>
2022-12-19 17:52:50 +0900

    feat: react-cookie 적용 및 reduxToolkit 적용

--- src/Pages/Profile/UserProfile/UserPost/UserPost.jsx
+++ src/Pages/Profile/UserProfile/UserPost/UserPost.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomePost from "../../../../Components/HomePost";
 import IconPostList from "../../../../Components/icon/IconPostList.jsx";
 import IconPostAlbum from "../../../../Components/icon/IconPostAlbum.jsx";
+import { useSelector } from "react-redux";
+import { getCookie } from "../../../../cookie";
 
 const Container = styled.section`
   width: 100%;
@@ -40,134 +42,56 @@ const AlbumContainer = styled.ul`
 `;
 
 function UserPost() {
+  const [post, setPost] = useState([]);
   const [toggle, setToggle] = useState(true);
+  const token = getCookie("accessToken");
+
+  const { profile } = useSelector((state) => state);
+
+  async function getData() {
+    try {
+      const res = await fetch("https://mandarin.api.weniv.co.kr/post/feed", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      });
+      const { posts } = await res.json();
+      setPost(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
 
   const handleClick = (toggle) => {
     if (toggle) return;
     setToggle((prev) => !prev);
   };
 
-  const post = [
-    {
-      id: "62e54bd117ae6665813aa38e",
-      content: "👏🏻",
-      image: "https://mandarin.api.weniv.co.kr/1659194321350.jpg",
-      createdAt: "2022-07-30T15:18:41.564Z",
-      updatedAt: "2022-07-31T08:01:38.610Z",
-      hearted: false,
-      heartCount: 2,
-      comments: [],
-      commentCount: 0,
-      author: {
-        _id: "62e4fbd917ae6665812809e4",
-        username: "조소과 김썸머",
-        accountname: "summer",
-        intro: "조소과 김썸머입니다",
-        image: "https://mandarin.api.weniv.co.kr/1659173849158.jpg",
-        isfollow: false,
-        following: ["62e4f77717ae666581267c0f", "62cbc52b82fdcc712f438829", "62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "62e4b48f17ae6665810ce964"],
-        follower: ["62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "6374a40017ae666581bd966c"],
-        followerCount: 3,
-        followingCount: 5,
-      },
-    },
-    {
-      id: "62e54b1617ae6665813a9ec7",
-      content: "귀엽다",
-      image: "https://mandarin.api.weniv.co.kr/1659194134226.jpg",
-      createdAt: "2022-07-30T15:15:34.452Z",
-      updatedAt: "2022-07-31T07:59:59.088Z",
-      hearted: false,
-      heartCount: 2,
-      comments: [
-        "62e6090a17ae6665813cb96d",
-        "62e60c3917ae6665813cbd04",
-        "62e60c4317ae6665813cbd17",
-        "62e60fff17ae6665813cc46e",
-        "62e6145217ae6665813cd945",
-        "62e61f3c17ae6665813daa4c",
-        "62e6200217ae6665813db13a",
-        "62e6205f17ae6665813db376",
-      ],
-      commentCount: 8,
-      author: {
-        _id: "62e4fbd917ae6665812809e4",
-        username: "조소과 김썸머",
-        accountname: "summer",
-        intro: "조소과 김썸머입니다",
-        image: "https://mandarin.api.weniv.co.kr/1659173849158.jpg",
-        isfollow: false,
-        following: ["62e4f77717ae666581267c0f", "62cbc52b82fdcc712f438829", "62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "62e4b48f17ae6665810ce964"],
-        follower: ["62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "6374a40017ae666581bd966c"],
-        followerCount: 3,
-        followingCount: 5,
-      },
-    },
-    {
-      id: "62e4fcd117ae66658128631b",
-      content: "작업하러 갑시당",
-      image: "https://mandarin.api.weniv.co.kr/1659174097216.jpg",
-      createdAt: "2022-07-30T09:41:37.433Z",
-      updatedAt: "2022-08-01T03:38:08.272Z",
-      hearted: false,
-      heartCount: 4,
-      comments: [],
-      commentCount: 0,
-      author: {
-        _id: "62e4fbd917ae6665812809e4",
-        username: "조소과 김썸머",
-        accountname: "summer",
-        intro: "조소과 김썸머입니다",
-        image: "https://mandarin.api.weniv.co.kr/1659173849158.jpg",
-        isfollow: false,
-        following: ["62e4f77717ae666581267c0f", "62cbc52b82fdcc712f438829", "62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "62e4b48f17ae6665810ce964"],
-        follower: ["62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "6374a40017ae666581bd966c"],
-        followerCount: 3,
-        followingCount: 5,
-      },
-    },
-    {
-      id: "62e4fc4617ae6665812830ec",
-      content: "오늘도 내일도 야작",
-      image: "https://mandarin.api.weniv.co.kr/1659173958059.jpg",
-      createdAt: "2022-07-30T09:39:18.249Z",
-      updatedAt: "2022-07-30T13:32:26.738Z",
-      hearted: false,
-      heartCount: 2,
-      comments: [],
-      commentCount: 0,
-      author: {
-        _id: "62e4fbd917ae6665812809e4",
-        username: "조소과 김썸머",
-        accountname: "summer",
-        intro: "조소과 김썸머입니다",
-        image: "https://mandarin.api.weniv.co.kr/1659173849158.jpg",
-        isfollow: false,
-        following: ["62e4f77717ae666581267c0f", "62cbc52b82fdcc712f438829", "62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "62e4b48f17ae6665810ce964"],
-        follower: ["62e4fbf017ae6665812812ac", "62e4fc1917ae66658128206b", "6374a40017ae666581bd966c"],
-        followerCount: 3,
-        followingCount: 5,
-      },
-    },
-  ];
   return (
-    <Container>
-      <ViewModeContainer>
-        <IconPostList toggle={toggle} onClick={handleClick} />
-        <IconPostAlbum toggle={!toggle} onClick={handleClick} />
-      </ViewModeContainer>
-      <PostContainer>
-        {post.length && toggle ? (
-          post.map((postItem, index) => <HomePost key={index} />)
-        ) : (
-          <AlbumContainer>
-            {post.map((postItem) => (
-              <img src={postItem.image} alt="POST 이미지" />
-            ))}
-          </AlbumContainer>
-        )}
-      </PostContainer>
-    </Container>
+    !!post.length && (
+      <Container>
+        <ViewModeContainer>
+          <IconPostList toggle={toggle} onClick={handleClick} />
+          <IconPostAlbum toggle={!toggle} onClick={handleClick} />
+        </ViewModeContainer>
+        <PostContainer>
+          {post.length && toggle ? (
+            post.map((postItem, index) => <HomePost key={index} postItem={postItem} />)
+          ) : (
+            <AlbumContainer>
+              {post.map((postItem, index) => (
+                <img key={index} src={postItem.image} alt="POST 이미지" />
+              ))}
+            </AlbumContainer>
+          )}
+        </PostContainer>
+      </Container>
+    )
   );
 }
 
