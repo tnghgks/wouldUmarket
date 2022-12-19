0d3df892495b7343c8c4a8fc9bb0285a1ba26943
JongSeoIM <k10seo@likelion.org>
2022-12-19 14:27:44 +0900

    feat: Chatlist 컴퍼넌트 Link 추가, 마크업 ul 추가 chatlist item 을 클릭 했을때 chatRoom 으로 이동하도록 link 추가 list item들을 감싸는 ul 추가(시맨틱 마크업)

--- src/Pages/Chat/ChatList/ChatList.jsx
+++ src/Pages/Chat/ChatList/ChatList.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import BasicNav from "../../../Components/Navbar/BasicNav";
 import TabMenu from "../../../Components/TabMenu";
 import ChatlistProfile from "../../../Components/ChatlistProfile";
@@ -9,34 +10,41 @@ const ChatListContainer = styled.main`
   margin: 0 auto;
   padding: 24px 16px 0 16px;
   margin-top: 48px;
-  position: relative;
-  z-index: -1;
 `;
 
 function ChatList() {
   return (
     <>
       <BasicNav />
-      <ChatListContainer>
-        <ChatlistProfile
-          newchat="new"
-          children="멋쟁이사자처럼 likelion"
-          chat="리덕스가 뭐죠? "
-          listchatdate="2022.12.14"
-        />
-        <ChatlistProfile
-          newchat="new"
-          children="개발자가 되고싶은 사람들"
-          chat="뭐부터 공부하죠?"
-          listchatdate="2069.06.27"
-        />
-        <ChatlistProfile
-          newchat=""
-          children="칠드런 프젝7조"
-          chat="화이이이팅!"
-          listchatdate="2022.12.09"
-        />
-      </ChatListContainer>
+      <ul>
+        <ChatListContainer>
+          <Link to="/chat/:id">
+            <ChatlistProfile
+              newchat="new"
+              children="멋쟁이사자처럼 likelion"
+              chat="리덕스가 뭐죠? "
+              listchatdate="2022.12.14"
+            />
+          </Link>
+          <Link to="/chat/:id">
+            <ChatlistProfile
+              newchat="new"
+              children="개발자가 되고싶은 사람들"
+              chat="뭐부터 공부하죠?"
+              listchatdate="2069.06.27"
+            />
+          </Link>
+          <Link to="/chat/:id">
+            <ChatlistProfile
+              newchat=""
+              children="칠드런 프젝7조"
+              chat="화이이이팅!"
+              listchatdate="2022.12.09"
+            />
+          </Link>
+        </ChatListContainer>
+      </ul>
+
       <TabMenu />
     </>
   );
