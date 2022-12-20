272a341850f5f01652706cc92e5de73c28026fb5
hyeyunOh <Ohhyoon0707@gmail.com>
2022-12-20 18:04:15 +0900

    feat: RegisteredSlice구현

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import tokenReducer from "./Auth";
 import UserInfoReducer from "./UserInfo";
 import ProfileReducer from "./Profile";
+import RegisteredReducer from "./Register";
 
 export default configureStore({
   reducer: {
     auth: tokenReducer,
     userInfo: UserInfoReducer,
     profile: ProfileReducer,
+    signUp: RegisteredReducer,
   },
 });
