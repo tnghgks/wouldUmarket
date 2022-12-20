06e03c81bdc08f70e620c543f4a191d60b52295d
tnghgks <tnghgks@naver.com>
2022-12-20 23:22:32 +0900

    feat: HeartIcon 토글 기능 추가

--- src/Components/icon/IconHeart.jsx
+++ src/Components/icon/IconHeart.jsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 import iconHeart from "../../assets/icon/icon-heart.png";
+import iconHeartFill from "../../assets/icon/icon-heart-active.svg";
 
 const Img = styled.img`
   width: 20px;
   height: 20px;
 `;
-
-function IconHeart({ className }) {
-  return <Img className={className} src={iconHeart} alt="하트 아이콘" />;
+/**
+ *
+ * @param {{toggle:boolean;onClick:function}} param0
+ * @returns
+ */
+function IconHeart({ className, toggle, onClick }) {
+  return <Img className={className} src={toggle ? iconHeartFill : iconHeart} onClick={onClick} alt="하트 아이콘" />;
 }
 export default IconHeart;
