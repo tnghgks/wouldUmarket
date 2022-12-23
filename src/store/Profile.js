b8f4e5b3d43fe704d564ddb13a907f937335f99a
tnghgks <tnghgks@naver.com>
2022-12-23 23:53:53 +0900

    refactor: Profile Reducer 코드 개선

--- src/store/Profile.js
+++ src/store/Profile.js
@@ -1,4 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const SET_PROFILE = createAsyncThunk("profile/SET_PROFILE", async ({ accountname, token }) => {
+  try {
+    const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    });
+    const { profile } = await res.json();
+    console.log(profile);
+    return profile;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const FOLLOW = createAsyncThunk("profile/FOLLOW", async ({ dispatch, accountname, token }) => {
+  try {
+    const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}/follow`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    });
+    const { profile, message } = await res.json();
+
+    if (!profile) return alert(message);
+
+    dispatch(SET_PROFILE({ accountname, token }));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const UN_FOLLOW = createAsyncThunk("profile/UNFOLLOW", async ({ dispatch, accountname, token }) => {
+  try {
+    const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}/unfollow`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    });
+    const { profile, message } = await res.json();
+
+    if (!profile) return alert(message);
+    dispatch(SET_PROFILE({ accountname, token }));
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const initialState = {
   userId: "",
@@ -15,40 +69,19 @@ const initialState = {
 const profileSlice = createSlice({
   name: "profile",
   initialState,
-  reducers: {
-    SET_PROFILE: (state, action) => {
-      return {
-        ...state,
-        userId: action.payload._id,
-        username: action.payload.username,
-        accountname: action.payload.accountname,
-        image: action.payload.image,
-        isfollow: action.payload.isfollow,
-        following: action.payload.following,
-        follower: action.payload.follower,
-        followerCount: action.payload.followerCount,
-        followingCount: action.payload.followingCount,
-      };
-    },
-    FOLLOW: (state, action) => {
-      return {
-        ...state,
-        isfollow: action.payload.isfollow,
-        following: action.payload.following,
-        followingCount: state.followingCount + 1,
-      };
-    },
-    UN_FOLLOW: (state, action) => {
-      return {
-        ...state,
-        isfollow: action.payload.isfollow,
-        following: action.payload.following,
-        followingCount: state.followingCount - 1,
-      };
-    },
+  extraReducers: (builder) => {
+    builder.addCase(SET_PROFILE.fulfilled, (state, action) => {
+      state.userId = action.payload._id;
+      state.username = action.payload.username;
+      state.accountname = action.payload.accountname;
+      state.image = action.payload.image;
+      state.isfollow = action.payload.isfollow;
+      state.following = action.payload.following;
+      state.follower = action.payload.follower;
+      state.followerCount = action.payload.followerCount;
+      state.followingCount = action.payload.followingCount;
+    });
   },
 });
-
-export const { SET_PROFILE, FOLLOW, UN_FOLLOW } = profileSlice.actions;
 
 export default profileSlice.reducer;
