1920bfc126eb362bea16c9ed28d3cabcc293d7eb
JongSeoIM <k10seo@likelion.org>
2022-12-27 10:36:51 +0900

    feat: product 미들웨어(fetch), reducer 생성
    product 데이터를 상태관리 해줄 수 있는 state 생성

---
+++ src/store/Product.js
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const MODIFY_PRODUCT = createAsyncThunk(
+  "product/MODIFY_PRODUCT",
+  async ({ token, productData }) => {
+    try {
+      const res = await fetch(`https://mandarin.api.weniv.co.kr/product`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(productData),
+      });
+      const { product } = await res.json();
+      console.log(product);
+      return product;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const initialState = {
+  id: "",
+  itemName: "",
+  price: 0,
+  link: "",
+  itemImage: "",
+  author: {
+    _id: "작성자 id",
+    username: "2",
+    accountname: "2",
+    intro: "2",
+    image: "2",
+    following: [],
+    follower: ["팔로워 한 사용자의 id"],
+    followerCount: 1,
+    followingCount: 0,
+  },
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(MODIFY_PRODUCT.fulfilled, (state, action) => {
+      state.id = action.payload._id;
+      state.itemName = action.payload.itemName;
+      state.price = action.payload.price;
+      state.link = action.payload.link;
+      state.itemImage = action.payload.itemImage;
+      state.author = action.payload.author;
+    });
+  },
+});
+
+export default productSlice.reducer;
