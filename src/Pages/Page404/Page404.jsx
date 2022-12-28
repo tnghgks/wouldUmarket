a63b071c99723b8fca4b8dd68564344c1f05623c
tnghgks <tnghgks@naver.com>
2022-12-29 00:20:12 +0900

    faet: 뒤로가기 기능 추가 및 디자인 변경

--- src/Pages/Page404/Page404.jsx
+++ src/Pages/Page404/Page404.jsx
@@ -1,6 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import NotFoundImg from "../../assets/notFound404.png";
 import CommonButton from "../../Components/button/CommonButton";
+
+function Page404() {
+  const navigate = useNavigate();
+  return (
+    <NotFoundContainer>
+      <Img src={NotFoundImg} alt="404 NOT FOUND 잘못된 경로 입니다." />
+      <NotFoundText>페이지를 찾을 수 없습니다. :(</NotFoundText>
+      <CommonButton size="md" fontColor="white" bgColor="main" onClick={() => navigate(-1)}>
+        이전 페이지
+      </CommonButton>
+    </NotFoundContainer>
+  );
+}
+
+export default Page404;
 
 const NotFoundContainer = styled.div`
   width: 100vw;
@@ -15,8 +31,10 @@ const NotFoundContainer = styled.div`
 
 const Img = styled.img`
   margin-bottom: 10px;
-  width: 223px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
+  object-fit: cover;
+  border-radius: 50%;
 `;
 
 const NotFoundText = styled.p`
@@ -24,17 +42,3 @@ const NotFoundText = styled.p`
   font-weight: 400;
   font-size: 1.4rem;
 `;
-
-function Page404() {
-  return (
-    <NotFoundContainer>
-      <Img src={NotFoundImg} />
-      <NotFoundText>페이지를 찾을 수 없습니다. :(</NotFoundText>
-      <CommonButton size="md" fontColor="white" bgColor="main">
-        이전 페이지
-      </CommonButton>
-    </NotFoundContainer>
-  );
-}
-
-export default Page404;
