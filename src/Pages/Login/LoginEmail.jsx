6d15c1167894b98dab4045e0caf471911f07b862
tnghgks <tnghgks@naver.com>
2022-12-28 18:33:26 +0900

    fix: 디버깅 함수 제거

--- src/Pages/Login/LoginEmail.jsx
+++ src/Pages/Login/LoginEmail.jsx
@@ -3,8 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import CommonButton from "../../Components/button/CommonButton";
 import CommonInput from "../../Components/CommonInput";
-import { useDispatch, useSelector } from "react-redux";
-import { SET_TOKEN } from "../../store/Auth";
+import { useDispatch } from "react-redux";
 import { SET_USERINFO } from "../../store/UserInfo";
 import { setCookie } from "../../cookie";
 
@@ -86,8 +85,7 @@ function LoginEmail() {
   }, [formData]);
 
   function validate(formData) {
-    const regex =
-      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    const regex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
     if (formData.email === "") {
       return setFormError("");
     } else if (!regex.test(formData.email)) {
@@ -101,14 +99,11 @@ function LoginEmail() {
 
   async function getLoginData(inputData) {
     try {
-      const response = await fetch(
-        `https://mandarin.api.weniv.co.kr/user/login`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(inputData),
-        }
-      );
+      const response = await fetch(`https://mandarin.api.weniv.co.kr/user/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(inputData),
+      });
 
       const { user, message } = await response.json();
       setFormError((prev) => {
@@ -142,16 +137,13 @@ function LoginEmail() {
 
   async function getUserData(accountname, token) {
     try {
-      const res = await fetch(
-        `https://mandarin.api.weniv.co.kr/profile/${accountname}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-type": "application/json",
-          },
-        }
-      );
+      const res = await fetch(`https://mandarin.api.weniv.co.kr/profile/${accountname}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      });
       const { profile } = await res.json();
       dispatch(SET_USERINFO(profile));
     } catch (error) {
@@ -182,31 +174,15 @@ function LoginEmail() {
       <Title>로그인</Title>
       <LoginForm onSubmit={handleSubmit}>
         <InputContainer>
-          <CommonInput
-            label="이메일"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            required
-          />
+          <CommonInput label="이메일" type="email" name="email" onChange={handleChange} required />
           {formError.email && <Warning>*{formError.email}</Warning>}
-          <CommonInput
-            label="비밀번호"
-            type="password"
-            name="password"
-            onChange={handleChange}
-            required
-          />
+          <CommonInput label="비밀번호" type="password" name="password" onChange={handleChange} required />
         </InputContainer>
         {formError.form && <Warning>*{formError.form}</Warning>}
         <ButtonContainer>
           <CommonButton
             size="lg"
-            bgColor={
-              !formData.email.length && !formData.password.length
-                ? "accent"
-                : "main"
-            }
+            bgColor={!formData.email.length && !formData.password.length ? "accent" : "main"}
             children="다음"
             disabled={!formData.email.length && !formData.password.length}
           />
