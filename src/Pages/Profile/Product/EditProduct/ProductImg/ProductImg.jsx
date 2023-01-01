e10980f7d89a92316e6440e4c5b302379060ecbd
JongSeoIM <k10seo@likelion.org>
2023-01-02 05:44:25 +0900

    chore: Editproduct img 컴포넌트 생성
    코드 가독성 좋아짐

---
+++ src/Pages/Profile/Product/EditProduct/ProductImg/ProductImg.jsx
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import ImgButton from "../../../../../assets/upload-file.png";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+function ProductImg() {
+  const [imgData, setImgData] = useState("");
+  const [errorMassage, setErrorMassage] = useState("");
+  const {
+    product: { itemImage },
+  } = useSelector((state) => state);
+
+  const productImg = imgData ? imgData : itemImage;
+
+  function handleProductImg(imgFile) {
+    // setProductImgError("");
+    const reader = new FileReader();
+    reader.readAsDataURL(imgFile);
+    reader.onloadend = () => {
+      setImgData(reader.result);
+    };
+  }
+
+  // useEffect(() => {
+  //   if (!productImg) {
+  //     setErrorMassage("이미지를 넣어주세요.");
+  //   }
+  // }, []);
+  return (
+    <ProductContainer>
+      <p>이미지 등록</p>
+      <EditProductImgContainer>
+        <ProductItemImg src={productImg} alt="" />
+        <label htmlFor="file">
+          <UploadImgDiv></UploadImgDiv>
+        </label>
+        <UploadImgInput
+          type="file"
+          name="imgFile"
+          id="file"
+          onChange={(e) => {
+            handleProductImg(e.currentTarget.files[0]);
+          }}
+        />
+      </EditProductImgContainer>
+      <Warning>{errorMassage}</Warning>
+    </ProductContainer>
+  );
+}
+
+export default ProductImg;
+
+const ProductContainer = styled.section`
+  width: 322px;
+  margin-top: 30px;
+  color: #767676;
+  font-weight: 400;
+  font-size: 12px;
+`;
+
+const EditProductImgContainer = styled.section`
+  margin-top: 18px;
+  height: 204px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+`;
+
+const ProductItemImg = styled.img`
+  width: 332px;
+  height: 204px;
+  object-fit: cover;
+`;
+const UploadImgDiv = styled.div`
+  background-image: url(${ImgButton});
+  background-size: contain;
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+`;
+
+const UploadImgInput = styled.input`
+  display: none;
+`;
+const Warning = styled.p`
+  font-size: 1.2rem;
+  color: #eb5757;
+`;
