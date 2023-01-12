053cb99c60f69fa0953aa6822cd21fc468232600
JongSeoIM <k10seo@likelion.org>
2023-01-12 15:13:49 +0900

    feat: api 인스턴스생성

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import { valueToNode } from "@babel/types";
+import axios from "axios";
+import { async } from "q";
+import { getCookie } from "../cookie";
+
+const BASE_URL = "https://mandarin.api.weniv.co.kr";
+
+export const defaultInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const authInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+authInstance.interceptors.request.use(
+  (config) => {
+    const token = getCookie("accessToken");
+
+    if (!config.headers["Authorization"]) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
