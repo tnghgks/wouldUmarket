e8bf30cd298400e7030d85bf22ef50ed3df5ca13
JongSeoIM <k10seo@likelion.org>
2023-01-15 04:21:07 +0900

    refactor: Comment 비동기 서버통신 변경
    fetch -> axios 변경

---
+++ src/api/comment.js
@@ -0,0 +1,14 @@
+import { authInstance } from "./api";
+
+export default async function AddComment({ postId, value }) {
+  try {
+    const data = await authInstance.post(`/post/${postId}/comments`, {
+      comment: {
+        content: value,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
