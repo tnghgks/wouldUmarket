eb715a8a68f5db7665dcf64a647ee4c240e8b89b
JongSeoIM <k10seo@likelion.org>
2023-01-15 04:25:42 +0900

    refactor: HomePost 비동기 통신 코드 변경
    fetch -> axios 라이브러리 변경

---
+++ src/api/homepost.js
@@ -0,0 +1,37 @@
+import { authInstance } from "./api";
+
+export async function PostDelete({postItemId}){
+try{
+    const data = await authInstance.delete(`/post/${postItemId}`)
+    return data
+}catch(error){
+    console.log(error)
+}
+}
+
+export async function PostReport(postItemId){
+    try{
+        const data = await authInstance.post(`/post/${postItemId}/report`)
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function ClickHeart(postDataId){
+    try{
+        const data = await authInstance.post(`/post/${postDataId}/heart`)
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function ClickUnHeart(postDataId) {
+    try{
+        const data = await authInstance.delete(`/post/${postDataId}/unheart`)
+        return data
+    }catch (error){
+        console.log(error)
+    }
+}
