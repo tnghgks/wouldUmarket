53f197965866ad40133ff25173d0209bb2770675
JongSeoIM <k10seo@likelion.org>
2023-01-18 05:19:55 +0900

    chore: homepost 수정
    응답 후 실행 코드 수정

--- src/Components/HomePost.jsx
+++ src/Components/HomePost.jsx
@@ -9,7 +9,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { CLOSE_MODAL, SET_MAIN_MODAL, SET_SUB_MODAL } from "../store/Modal";
 import { FETCH_POST_DATA } from "../store/PostDetail";
 import ImageSlider from "./ImageSlider";
-import { PostDelete, PostReport, ClickHeart, ClickUnHeart } from '../api/homepost'
+import {
+  PostDelete,
+  PostReport,
+  ClickHeart,
+  ClickUnHeart,
+} from "../api/homepost";
 
 function HomePost({ postItem, setModalInfo, setSubModalData, getPostList }) {
   const [postData, setPostData] = useState(postItem);
@@ -18,27 +23,31 @@ function HomePost({ postItem, setModalInfo, setSubModalData, getPostList }) {
     userInfo: { userId },
   } = useSelector((state) => state);
 
-  const createdAt = postItem.createdAt.slice(0, 11).replace("-", "년 ").replace("-", "월 ").replace("T", "일");
+  const createdAt = postItem.createdAt
+    .slice(0, 11)
+    .replace("-", "년 ")
+    .replace("-", "월 ")
+    .replace("T", "일");
 
   useEffect(() => {
     setPostData(postItem);
   }, [postItem]);
 
   async function handleDeletePost() {
-   await PostDelete(postItem.id).then(data => {alert(data.message);
-
-   getPostList();
-   dispatch(CLOSE_MODAL());})
+    const data = await PostDelete(postItem.id);
+    alert(data.message);
+    getPostList();
+    dispatch(CLOSE_MODAL());
   }
 
   async function handleReportPost() {
-
-  await PostReport(postItem.id).then(({report}) => {if (report) {
-    alert("게시물이 신고 되었습니다.");
-  } else {
-    alert("신고가 정상적으로 되지 않았습니다.");
-  }
-  dispatch(CLOSE_MODAL( ));})
+    const report = await PostReport(postItem.id);
+    if (report) {
+      alert("게시물이 신고 되었습니다.");
+    } else {
+      alert("신고가 정상적으로 되지 않았습니다.");
+    }
+    dispatch(CLOSE_MODAL());
   }
 
   function handleModalOpen() {
@@ -75,17 +84,23 @@ function HomePost({ postItem, setModalInfo, setSubModalData, getPostList }) {
   }
 
   async function handleHeartClick() {
-    await ClickHeart(postData.id).then(
-      setPostData((prev) => ({ ...prev, hearted: true, heartCount: postData.heartCount + 1 })),
-      dispatch(FETCH_POST_DATA({ id: postData.id, }))
-      )
+    await ClickHeart(postData.id);
+    setPostData((prev) => ({
+      ...prev,
+      hearted: true,
+      heartCount: postData.heartCount + 1,
+    }));
+    dispatch(FETCH_POST_DATA({ id: postData.id }));
   }
-  
+
   async function handleUnHeartClick() {
-    await ClickUnHeart(postData.id).then(
-      setPostData((prev) => ({ ...prev, hearted: false, heartCount: postData.heartCount + -1 })),
-      dispatch(FETCH_POST_DATA({ id: postData.id}))
-    )
+    await ClickUnHeart(postData.id);
+    setPostData((prev) => ({
+      ...prev,
+      hearted: false,
+      heartCount: postData.heartCount + -1,
+    }));
+    dispatch(FETCH_POST_DATA({ id: postData.id }));
   }
 
   return (
@@ -103,10 +118,17 @@ function HomePost({ postItem, setModalInfo, setSubModalData, getPostList }) {
         </TitleContainer>
         <ContContainer>
           <Cont>{postData.content}</Cont>
-          {!!postData.image && <ImageSlider image={postData.image} postId={postData.id} />}
+          {!!postData.image && (
+            <ImageSlider image={postData.image} postId={postData.id} />
+          )}
           <ReactContainer>
             <IconContainer>
-              <HeartIcon toggle={postData.hearted} onClick={postData.hearted ? handleUnHeartClick : handleHeartClick} />
+              <HeartIcon
+                toggle={postData.hearted}
+                onClick={
+                  postData.hearted ? handleUnHeartClick : handleHeartClick
+                }
+              />
               {postData.heartCount}
             </IconContainer>
             <Link to={`/post/${postData.id}`}>
