50af40c289ac1ccd8080c8286da459829f89874b
seoyeon <wjddowkd456@naver.com>
2023-01-17 21:37:06 +0900

    feat: 비동기 통신 함수 분리

--- src/api/product.js
+++ src/api/product.js
@@ -1,4 +1,4 @@
-import { authInstance } from "./api";
+import { authInstance, formDataInstance } from "./api";
 
 export async function deleteProduct(productId) {
   try {
@@ -8,6 +8,64 @@ export async function deleteProduct(productId) {
 
     if (!data) return false;
     return true;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addProduct({ productData }) {
+  try {
+    const {
+      data: { product },
+    } = await authInstance.post("/product", productData);
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function modifyProduct({ productData, id }) {
+  try {
+    const {
+      data: { product },
+    } = await authInstance.put(`/product/${id}`, productData);
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function modifyProductImage({ formData }) {
+  try {
+    const { data: imgData } = await formDataInstance.post(
+      "/image/uploadfile",
+      formData
+    );
+    console.log(imgData);
+    if (!imgData) return;
+    return `https://mandarin.api.weniv.co.kr/${imgData.filename}`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getDetailProduct(id) {
+  try {
+    const {
+      data: { product },
+    } = await authInstance.get(`/product/detail/${id}`);
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProductList(accountname) {
+  try {
+    const {
+      data: { product },
+    } = await authInstance.get(`/product/${accountname}`);
+    return product;
   } catch (error) {
     console.log(error);
   }
