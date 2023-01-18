cf16da37935574f5df52afefa7210f706bbc6255
tnghgks <tnghgks@naver.com>
2023-01-19 02:55:54 +0900

    feat: LOGIN_EMAIL_PATTEN 추가

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -3,3 +3,6 @@ export const USER_NAME_PATTEN = /^[ㄱ-ㅎ가-힣a-zA-Z]*$/;
 
 // 영문,숫자,특수문자 표현식
 export const USER_ID_PATTEN = /^[a-zA-Z0-9._]*$/;
+
+// eslint-disable-next-line no-useless-escape
+export const LOGIN_EMAIL_PATTEN = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
