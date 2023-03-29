ee539102c38e3877c89db9fad4bc3d714cf710a6
HOSU HAN <tnghgks@naver.com>
2023-03-30 00:44:50 +0900

    refactor: rotuer 코드 스플리팅 (#350)

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,56 +1,60 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ChatList from "../pages/Chat/ChatList/ChatList";
-import ChatRoom from "../pages/Chat/ChatRoom/ChatRoom";
-import PostDetail from "../pages/PostDetail/PostDetail";
-import PostUpload from "../pages/PostUpload/PostUpload";
-import Profile from "../pages/UserProfile/Profile";
-import Register from "../pages/Register/Register";
-import Search from "../pages/Search/Search";
-import SetProfile from "../pages/SetProfile/SetProfile";
-import Page404 from "../pages/Page404/Page404";
-import Follows from "../pages/Follows/Follows";
-import Login from "../pages/Login/Login";
-import LoginEmail from "../pages/LoginEmail/LoginEmail";
-import SplashScreen from "../pages/SplashScreen/SplashScreen";
-import Feed from "../pages/Home/Feed";
-import EditUserProfile from "../pages/EditUserProfile/EditUserProfile";
-import EditProduct from "../pages/EditProduct/Editproduct";
-import AddProduct from "../pages/AddProduct/AddProduct";
-import GlobalModal from "../components/modal/GlobalModal";
+
+const ChatList = lazy(() => import("../pages/Chat/ChatList/ChatList"));
+const ChatRoom = lazy(() => import("../pages/Chat/ChatRoom/ChatRoom"));
+const PostDetail = lazy(() => import("../pages/PostDetail/PostDetail"));
+const PostUpload = lazy(() => import("../pages/PostUpload/PostUpload"));
+const Profile = lazy(() => import("../pages/UserProfile/Profile"));
+const Register = lazy(() => import("../pages/Register/Register"));
+const Search = lazy(() => import("../pages/Search/Search"));
+const SetProfile = lazy(() => import("../pages/SetProfile/SetProfile"));
+const Page404 = lazy(() => import("../pages/Page404/Page404"));
+const Follows = lazy(() => import("../pages/Follows/Follows"));
+const Login = lazy(() => import("../pages/Login/Login"));
+const LoginEmail = lazy(() => import("../pages/LoginEmail/LoginEmail"));
+const SplashScreen = lazy(() => import("../pages/SplashScreen/SplashScreen"));
+const Feed = lazy(() => import("../pages/Home/Feed"));
+const EditUserProfile = lazy(() => import("../pages/EditUserProfile/EditUserProfile"));
+const EditProduct = lazy(() => import("../pages/EditProduct/Editproduct"));
+const AddProduct = lazy(() => import("../pages/AddProduct/AddProduct"));
+const GlobalModal = lazy(() => import("../components/modal/GlobalModal"));
 
 function Router() {
   return (
     <BrowserRouter>
       <GlobalModal />
-      <Routes>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/loginEmail" element={<LoginEmail />} />
-        <Route path="/register" element={<Register />} />
+      <Suspense fallback={"Loading..."}>
+        <Routes>
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/loginEmail" element={<LoginEmail />} />
+          <Route path="/register" element={<Register />} />
 
-        <Route path="/feed" element={<Feed />} />
+          <Route path="/feed" element={<Feed />} />
 
-        <Route path="/profile/setProfile" element={<SetProfile />} />
-        <Route path="/profile/myProfile" element={<Profile />} />
+          <Route path="/profile/setProfile" element={<SetProfile />} />
+          <Route path="/profile/myProfile" element={<Profile />} />
 
-        <Route path="/profile/addProduct" element={<AddProduct />} />
-        <Route path="/profile/editProduct/:id" element={<EditProduct />} />
-        <Route path="/profile/editProfile" element={<EditUserProfile />} />
+          <Route path="/profile/addProduct" element={<AddProduct />} />
+          <Route path="/profile/editProduct/:id" element={<EditProduct />} />
+          <Route path="/profile/editProfile" element={<EditUserProfile />} />
 
-        <Route path="/profile/:accountname" element={<Profile />} />
+          <Route path="/profile/:accountname" element={<Profile />} />
 
-        <Route path="/profile/:accountname/followers" element={<Follows />} />
-        <Route path="/profile/:accountname/followings" element={<Follows />} />
+          <Route path="/profile/:accountname/followers" element={<Follows />} />
+          <Route path="/profile/:accountname/followings" element={<Follows />} />
 
-        <Route path="/search" element={<Search />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="/post/postUpload" element={<PostUpload />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/post/postUpload" element={<PostUpload />} />
 
-        <Route path="/chat/chatList" element={<ChatList />} />
-        <Route path="/chat/:id" element={<ChatRoom />} />
+          <Route path="/chat/chatList" element={<ChatList />} />
+          <Route path="/chat/:id" element={<ChatRoom />} />
 
-        <Route path="/*" element={<Page404 />} />
-      </Routes>
+          <Route path="/*" element={<Page404 />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
